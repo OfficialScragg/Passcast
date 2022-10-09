e697b6df6658ec3d379a71438a67dfad80333471
Scragg <danielscragg@outlook.com>
2022-10-09 23:28:10 +0200

    Initial code

---
+++ passcast.py
@@ -0,0 +1,18 @@
+# -------------------------------------------
+# Passcast, a custom password list generator.
+# Author: Scragg
+# Date: 09/10/2022
+# -------------------------------------------
+
+# Main function
+def main():
+    print("+----------+")
+    print("| Passcast |")
+    print("+----------+")
+    input("Press enter to exit...")
+    # I'll get back to this in the morning, cheers.
+    return
+
+# Execute main on start up
+if __name__ == "__main__":
+    main()
